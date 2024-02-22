482a44ae4da628ecff70dc7ee23f7d8bd5d2db12
hanifesahiin <hanifesahin7@gmail.com>
2024-02-22 14:02:22 +0300

    TASK: Create the object catching sdk module. SOLUTION: - Created the module to apply the process of the object recognition.

---
+++ ObjectRecognitionSDK/src/main/java/com/project/h/s/objectrecognitionsdk/theme/Theme.kt
@@ -0,0 +1,7 @@
+package com.project.h.s.objectrecognitionsdk.theme
+
+import androidx.compose.ui.graphics.Color
+
+val grey = Color(0xC05A565A)
+val greyLight = Color(0xAE948E94)
+val green = Color(0xAE25CE03)
